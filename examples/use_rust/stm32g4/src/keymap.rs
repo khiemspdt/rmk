f92cf32acafa0ee29c5d074dfc9eb6187cc0106b
khiemspdt <quockhiem.le@tvsmotor.com>
2025-07-05 16:41:29 +0700

    add sample ADC

--- examples/use_rust/stm32g4/src/keymap.rs
+++ examples/use_rust/stm32g4/src/keymap.rs
@@ -1,23 +1,21 @@
 use rmk::action::KeyAction;
 use rmk::{a, k, layer, mo};
-pub(crate) const COL: usize = 3;
-pub(crate) const ROW: usize = 4;
+pub(crate) const COL: usize = 4;
+pub(crate) const ROW: usize = 3;
 pub(crate) const NUM_LAYER: usize = 2;
 
 #[rustfmt::skip]
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         layer!([
-            [k!(AudioVolUp), k!(B), k!(AudioVolDown)],
-            [k!(Kp4), k!(LShift), k!(Kp6)],
-            [mo!(1), k!(Kp2), k!(Kp3)],
-            [mo!(1), a!(No), k!(Kp0)]
+            [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(Kp0)],
+            [k!(Kp4), k!(LShift), k!(Kp6), k!(Kp1)],
+            [mo!(1), k!(Kp2), k!(Kp3), k!(Kp2)]
         ]),
         layer!([
-            [k!(Kp7), k!(Kp8), k!(Kp9)],
-            [k!(Kp4), k!(LCtrl), k!(Kp6)],
-            [mo!(1), k!(Kp2), k!(Kp3)],
-            [mo!(1), a!(No), k!(Kp0)]
+            [k!(Kp7), k!(Kp8), k!(Kp9), k!(Kp4)],
+            [k!(Kp4), k!(LCtrl), k!(Kp6), k!(Kp5)],
+            [mo!(1), k!(Kp2), k!(Kp3), k!(Kp6)]
         ]),
     ]
 }
--- examples/use_rust/stm32g4/src/main.rs
+++ examples/use_rust/stm32g4/src/main.rs
@@ -8,10 +8,13 @@ mod vial;
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_stm32::Peripheral;
+use embassy_stm32::adc::{Adc, AdcChannel as _, SampleTime};
 use embassy_stm32::gpio::{Input, Output};
 use embassy_stm32::peripherals::USB;
 use embassy_stm32::usb::{Driver, InterruptHandler};
-use embassy_stm32::{bind_interrupts, Config};
+use embassy_stm32::{Config, bind_interrupts};
+use embassy_time::Timer;
 use keymap::{COL, ROW};
 use rmk::channel::EVENT_CHANNEL;
 use rmk::config::{BehaviorConfig, ControllerConfig, RmkConfig, VialConfig};
@@ -28,50 +31,93 @@ bind_interrupts!(struct Irqs {
     USB_LP => InterruptHandler<USB>;
 });
 
+static mut DMA_BUF: [u16; 2] = [0; 2];
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    let mut read_buffer = unsafe { &mut DMA_BUF[..] };
+
     info!("RMK start!");
     // RCC config
-    let config = Config::default();
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL85,
+            divp: None,
+            divq: None,
+            // Main system clock at 170 MHz
+            divr: Some(PllRDiv::DIV2),
+        });
+        config.rcc.mux.adc12sel = mux::Adcsel::SYS;
+        config.rcc.sys = Sysclk::PLL1_R;
+    }
 
     // Initialize peripherals
     let p = embassy_stm32::init(config);
 
-    // Pin config
-    let (input_pins, output_pins) =
-        config_matrix_pins_stm32!(peripherals: p, input: [PB9, PB8, PB13, PB12], output: [PA8, PA9, PA10]);
+    // // Pin config
+    // let (input_pins, output_pins) =
+    //     config_matrix_pins_stm32!(peripherals: p, input: [PA4, PA5, PA6, PA7], output: [PB11,PB12, PB13, PB14]);
 
-    // Usb driver
-    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
+    // Configure PA4 as an analog pin
+    let mut adc = Adc::new(p.ADC2);
+    let mut dma = p.DMA1_CH2;
+    let mut adc_s0 = p.PA4.degrade_adc();
+    let mut adc_s1 = p.PA5.degrade_adc();
 
-    // Keyboard config
-    let rmk_config = RmkConfig {
-        vial_config: VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF),
-        ..Default::default()
-    };
+    loop {
+        adc.read(
+            &mut dma,
+            [
+                (&mut adc_s0, SampleTime::CYCLES247_5),
+                (&mut adc_s1, SampleTime::CYCLES247_5),
+            ]
+            .into_iter(),
+            &mut read_buffer,
+        )
+        .await;
 
-    // Initialize the keymap
-    let mut default_keymap = keymap::get_default_keymap();
-    let behavior_config = BehaviorConfig::default();
-    // let storage_config = StorageConfig::default();
+        let vrefint = read_buffer[0];
+        let measured = read_buffer[1];
+        info!("vrefint: {}", vrefint);
+        info!("measured: {}", measured);
+        Timer::after_millis(500).await;
+    }
 
-    let keymap = initialize_keymap(&mut default_keymap, behavior_config).await;
+    // // Usb driver
+    // let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
 
-    // Initialize the matrix + keyboard
-    let debouncer = DefaultDebouncer::<ROW, COL>::new();
-    let mut matrix = Matrix::<_, _, _, ROW, COL>::new(input_pins, output_pins, debouncer);
-    let mut keyboard = Keyboard::new(&keymap);
+    // // Keyboard config
+    // let rmk_config = RmkConfig {
+    //     vial_config: VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF),
+    //     ..Default::default()
+    // };
 
-    // Initialize the light controller
-    let mut light_controller: LightController<Output> = LightController::new(ControllerConfig::default().light_config);
+    // // Initialize the keymap
+    // let mut default_keymap = keymap::get_default_keymap();
+    // let behavior_config = BehaviorConfig::default();
+    // // let storage_config = StorageConfig::default();
 
-    // Start
-    join3(
-        keyboard.run(),
-        run_rmk(&keymap, driver, &mut light_controller, rmk_config),
-        run_devices! (
-            (matrix) => EVENT_CHANNEL,
-        ),
-    )
-    .await;
+    // let keymap = initialize_keymap(&mut default_keymap, behavior_config).await;
+
+    // // Initialize the matrix + keyboard
+    // let debouncer = DefaultDebouncer::<ROW, COL>::new();
+    // let mut matrix = Matrix::<_, _, _, ROW, COL>::new(input_pins, output_pins, debouncer);
+    // let mut keyboard = Keyboard::new(&keymap);
+
+    // // Initialize the light controller
+    // let mut light_controller: LightController<Output> = LightController::new(ControllerConfig::default().light_config);
+
+    // // Start
+    // join3(
+    //     keyboard.run(),
+    //     run_rmk(&keymap, driver, &mut light_controller, rmk_config),
+    //     run_devices! (
+    //         (matrix) => EVENT_CHANNEL,
+    //     ),
+    // )
+    // .await;
 }
